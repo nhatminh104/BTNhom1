a2fcec761b63a6c7d0fc9bd5bd34db870fa59f96
tai010120 <tai0101bin@gmail.com>
2024-09-24 21:25:15 +0700

    Update Bai5.java

--- Bai5.java
+++ Bai5.java
@@ -1,15 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Tuan3;
 
 import java.util.Scanner;
 
-/**
- *
- * @author ASUS
- */
 public class Bai5 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
