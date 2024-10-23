0242dee2e16da03d8e0817c611e543c26eeb8d0d
nhatminh104 <hanhatminh1042005@gmail.com>
2024-10-23 13:39:09 +0700

    Add files via upload

---
+++ baitapNhatMinh/BaiTapCaNhan/Test.java
@@ -0,0 +1,14 @@
+package TaiKhoanNganHang;
+
+public class Test {
+    public static void main(String[] args) {
+        TaiKhoanNH tknh = new TaiKhoanNH();
+        tknh.Nhap();
+        tknh.Xuat();
+        tknh.doiMatKhau();
+        tknh.kiemTraMatKhau();
+        tknh.guiTien();
+        tknh.rutTien();
+        tknh.kiemTraSoDu();
+    }
+}
